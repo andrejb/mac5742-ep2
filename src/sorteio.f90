07d2ca09ebf84383eaacdcdb177541ecc4a737ae
drebs <drebs@riseup.net>
2010-11-24 19:57:44 -0200

    ordenando a saida

--- src/sorteio.f90
+++ src/sorteio.f90
@@ -1,19 +1,23 @@
-! Adaptado do exemplo do pacote openmpi-doc do debian estável (lenny).
+! adaptado do exemplo do pacote openmpi-doc do debian estável (lenny).
 program sorteio
   use mpi
   implicit none
-  integer :: rank, size, tag, next, from, message, ierr, destination, i
+  integer :: rank, size, tag, next, prev, message, ierr, destination, i
   integer*4 :: timeArray(3) ! Contem a hora, minuto e segundos
   integer :: active
   character*10 :: cfrac
   real :: frac
 
-  ! Inicia o MPI
+
+  ! --------------------------------------------------------------------------
+  ! inicializacao.
+
+  ! inicia o MPI
   call MPI_INIT(ierr)
   call MPI_COMM_RANK(MPI_COMM_WORLD, rank, ierr)
   call MPI_COMM_SIZE(MPI_COMM_WORLD, size, ierr)
 
-  ! Verifica se o argumento da linha de comando faz sentido
+  ! verifica se o argumento da linha de comando faz sentido
   call getarg(1,cfrac)
   read(cfrac,*) frac
   if (frac .lt. 0.0 .or. frac .gt. 1.0) then
@@ -23,17 +27,17 @@ program sorteio
   end if
   message = ceiling(frac*size)
 
-  ! Inicializa o anel
+  ! inicializa o anel
   tag = 201
   next = mod((rank + 1), size)
-  from = mod((rank + size - 1), size)
+  prev = mod((rank + size - 1), size)
   active = 0
 
   ! inicializa numeros aleatorios
-  call itime(timeArray) ! Obtem a hora atual
+  call itime(timeArray) ! obtem a hora atual
   destination = int(rand(timeArray(1)+timeArray(2)+timeArray(3))) ! este primeiro valor eh jogado fora.
 
-  ! Se somos o processo 0, iniciamos o ciclo escolhendo um processo aleatorio e
+  ! se somos o processo 0, iniciamos o ciclo escolhendo um processo aleatorio e
   ! enviando quantos processos faltam mudar para o estado "Ativado".
   if (rank .eq. 0) then
      call get_random_int(0,size-1,destination)
@@ -41,6 +45,8 @@ program sorteio
      call MPI_SEND(message, 1, MPI_INTEGER, destination, tag, MPI_COMM_WORLD, ierr)
   endif
 
+
+  ! --------------------------------------------------------------------------
   ! loop principal:
   !
   ! enquanto (1), faça:
@@ -57,17 +63,12 @@ program sorteio
     call MPI_RECV(message, 1, MPI_INTEGER, MPI_ANY_SOURCE, tag, MPI_COMM_WORLD, &
                   MPI_STATUS_IGNORE, ierr)
 
-    ! Caso a mensagem seja -1, devo terminar.
+    ! caso a mensagem seja -1, devo terminar.
     if (message .eq. -1) then
-      if (active .eq. 1) then
-        print '(I4,A)', rank, ': ativo'
-      else
-        print '(I4,A)', rank, ':       inativo'
-      end if
       exit
     end if
 
-    ! Caso a mensagem seja 0, nao ha mais processos para serem alterado e eu
+    ! caso a mensagem seja 0, nao ha mais processos para serem alterado e eu
     ! devo avisar aos outros disto.
     if (message .eq. 0) then
       do i = 0, size-1
@@ -75,11 +76,11 @@ program sorteio
       end do
     end if
 
-    ! Se eu ja estou ativo passo a mensagem para frente.
+    ! se eu ja estou ativo passo a mensagem para frente.
     if (active .eq. 1) then
       !print *, '  ',rank,' -> ',destination,': faltam ',message,' processos.'
       call MPI_SEND(message, 1, MPI_INTEGER, next, tag, MPI_COMM_WORLD, ierr)
-    ! Caso contrario, mudo meu estado e repito o processo.
+    ! caso contrario, mudo meu estado e repito o processo.
     else
       active = 1
       message = message - 1
@@ -89,11 +90,43 @@ program sorteio
     end if
   end do
 
+
+  ! --------------------------------------------------------------------------
+  ! impressao de resultados.
+  call MPI_BARRIER(MPI_COMM_WORLD, ierr)
+
+  if (rank .ne. 0) then
+    do
+      call MPI_RECV(message, 1, MPI_INTEGER, prev, tag, MPI_COMM_WORLD, &
+                    MPI_STATUS_IGNORE, ierr)
+      ! descarta mensagens indesejadas
+      if (message .eq. -2) then
+        exit
+      end if
+    end do
+  end if
+
+  if (active .eq. 1) then
+    print '(I4,A)', rank, ': ativo'
+  else
+    print '(I4,A)', rank, ':       inativo'
+  end if
+
+  if (rank .ne. size-1) then
+    message = -2
+    call MPI_SEND(message, 1, MPI_INTEGER, next, tag, MPI_COMM_WORLD, ierr)
+  end if
+
   call MPI_FINALIZE(ierr)
+
 
 contains
 
-! Retorna um inteiro aleatorio entre start_int e end_int. Supoe que ja houve
+
+! ---------------------------------------------------------------------------- 
+! subrotina: get_random_int
+!
+! retorna um inteiro aleatorio entre start_int e end_int. Supoe que ja houve
 ! uma inicializacao previa dos numeros aleatorios.
 subroutine get_random_int(start_int, end_int, random_int)
   integer, intent(in) :: start_int, end_int
